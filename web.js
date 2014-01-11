ddbafcb4d3ac0d3ff94e54535031f61bf10e8c00
ashwin <ashwinbhawsar1983@gmail.com>
2014-01-11 19:05:54 +0530

    Ashwin - updated to read contentc from file-system.

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+    var fileContents = fs.readFile("index.html");
+  response.send(fileContents);
 });
 
 var port = process.env.PORT || 5000;
