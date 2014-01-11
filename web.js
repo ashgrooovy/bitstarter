90e38cd18a34269ea76a27dcb4084b3cb8184f8c
ashwin <ashwinbhawsar1983@gmail.com>
2014-01-11 19:14:36 +0530

    committing

--- web.js
+++ web.js
@@ -3,6 +3,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
+    var fs = require('fs');
     var fileContents = fs.readFile("index.html");
   response.send(fileContents);
 });
